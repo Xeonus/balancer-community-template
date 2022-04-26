08f428de5a47dc664078039b45f1431f2afc3b42
Xeonus <vanderbourg@gmail.com>
2022-04-26 15:38:30 +0200

    navbar routing bugfix

--- src/components/UI/NavBar.js
+++ src/components/UI/NavBar.js
@@ -1,20 +1,30 @@
 import React from "react";
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import { NavLink, Link } from "react-router-dom";
+import { NavLink, Link, useLocation} from "react-router-dom";
 //Navigation bar
 export default function NavBar(props) {
 
-    const [value, setValue] = React.useState("home");
+
+    //Obtain actively selcted route
+    let location = useLocation();
+
+    const [value, setValue] = React.useState("/home");
+
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
+
+    console.log("location", location.pathname.substring(1));
+    if (location && location.pathname.substring(1) !== value) {
+        setValue(location.pathname.substring(1));
+    }
 
     return (
         <div>
             <Tabs value={value} onChange={handleChange}>
                 <Tab color="primary" component={NavLink} to={'/home'} value={"home"} label="home" />
-                <Tab component={Link} to={'/notionIntegration'} value={"notion"} label="Notion" />
+                <Tab component={Link} to={'/notionIntegration'} value={"notionIntegration"} label="Notion" />
                 <Tab onClick={() => window.open('https://balancer.tools/')} value={"balancerTools"} label="balancer.tools" />
             </Tabs>
         </div>
